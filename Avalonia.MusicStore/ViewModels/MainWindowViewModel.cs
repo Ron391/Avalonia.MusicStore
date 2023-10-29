05218ffdeffc2e3492cc1b8c1fb3035903e9e13c
ronak <ronak@VECTOART-PC>
2023-10-29 12:16:15 +0100

    Add project files.

---
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+namespace Avalonia.MusicStore.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ICommand BuyMusicCommand { get; }
+        public MainWindowViewModel() {
+            BuyMusicCommand = ReactiveCommand.Create(() =>
+            {
+
+            });
+        }
+    }
+}
